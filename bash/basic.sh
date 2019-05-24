cd7f6bd35ecc6757e1050b7e07048d0f0fe4ed04
Spencer White <swhite215@live.com>
2019-05-24 10:59:18 -0400

    examples of basic linux commands

---
+++ bash/basic.sh
@@ -0,0 +1,34 @@
+# Basic Linux Commands
+
+ls # lists directory contents
+ls -l
+ls -al
+ls /bin
+
+cd # changes the current directory
+cd path/to/directory/file
+
+pwd # displays the present working director
+
+cat # concatenates and displays files
+cat filename
+
+echo # displays arguments to the screen
+echo $ENVIRONMENT_VARIABLE
+echo "string"
+echo $PATH # displays command search directories and order
+
+man # displays the online manual
+man command
+
+exit # exits the shell or your current session
+
+clear # clears the screen
+
+which # shows location or full path to the command that you're executing
+which node
+which npm
+which ls
+
+command --help
+command -h
