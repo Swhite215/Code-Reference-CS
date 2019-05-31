2740daa35ff3d4bce6627f5791fde9b56c6291ce
Spencer White <swhite215@live.com>
2019-05-31 13:12:23 -0400

    commands to view files

---
+++ bash/files.sh
@@ -0,0 +1,13 @@
+# Commands to View Files 
+
+cat myFile # display contents
+
+more myFile # browse through a text file
+
+less myFile # more features than more
+
+head myFile # output the first ten lines of a file
+
+tail myFile # output the last ten lines of a file
+
+tail -f myFile # follow the file as it is being written
