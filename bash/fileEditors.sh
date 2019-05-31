52fa41f904df8cd7aa3f65a21d9adc6e807f0d41
Spencer White <swhite215@live.com>
2019-05-31 13:23:25 -0400

    examples of starting various cli editors

---
+++ bash/fileEditors.sh
@@ -0,0 +1,9 @@
+# Examples of File Editors
+
+nano myFile # Use nano to edit a file
+
+vi myFile # Use vi to edit a file
+vim myFile # Use improved vi to edit a file
+view myFile # Use vim in read only mode
+
+emacs myFile # Use emacs to edit a file
