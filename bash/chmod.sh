e2c7cfd292fd63d8b65d05da5b6e283a0d961dc3
Spencer White <swhite215@live.com>
2019-05-31 12:57:57 -0400

    examples of using chmod

--- bash/chmod.sh
+++ bash/chmod.sh
@@ -1,7 +1,8 @@
 # Example CHMOD Commands, a tool for modifying permissions
 
 # Symbols
-chmod u=rwx,g=rwx,o=rwx myFile
+chmod u=rwx,g=rwx,o=rwx myFile # All groups all permissions
+chmod u-rwx,g+rwx,o=rwx myFile # User remove all permissions, group add all permissions, other set all permissions
 
 # Octal Numbers
 # User = 7, 4 (read) + 2 (write) + 1 (execute)
@@ -10,7 +11,8 @@ chmod u=rwx,g=rwx,o=rwx myFile
 chmod 754 myFile
 
 chmod 777 myFile # All groups all permissions
-chmod 444 myFile # All groups only read access
-chmod 111 myFile # All groups only execute access
-chmod 555 myFile # All groups read and execute access
-chmod 333 myFile # All groups write and execute access
+chmod 444 myFile # All groups only read permissions
+chmod 111 myFile # All groups only execute permissions
+chmod 555 myFile # All groups read and execute permissions
+chmod 333 myFile # All groups write and execute permissions
+chmod 000 myFIle # All groups no permissions
