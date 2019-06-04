62770d2bd9ec84e5ed826a80487b96aeb347491c
Spencer White <swhite215@live.com>
2019-06-04 16:22:58 -0400

    update to countdown end of round

--- bash/tabata.sh
+++ bash/tabata.sh
@@ -1,15 +1,16 @@
-# Script for my Tabata workout
-
 for VARIABLE in 1 2 3 4 5 6 7 8
 do
     for VAR in 1 2 3 4 5 6 7 8
     do
         say Workout $VARIABLE Round $VAR
-        sleep 20
-        say Break - Ten Second Rest
+        sleep 18
+        say Three
+        say Two
+        say One
+        say Break
         sleep 10
     done
 
-    say Break - One Minute Rest
+    say One Minute Rest
     sleep 60
 done
