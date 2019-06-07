a60570c875c68a11b24fb7d9b96d39b451af22f6
Spencer White <swhite215@live.com>
2019-06-07 09:57:03 -0400

    simple tar commands

---
+++ bash/tar.sh
@@ -0,0 +1,12 @@
+# Example Tar Commands, a tool for creating archive files called tarballs
+
+tar # flags
+# c, creates a tar archive
+# x, extracts file from the archive
+# t, displays table of contents
+# v, verbose
+# z, use compression
+# f, file
+
+tar cf file/folder # create a tar archive
+tar xf file.tar/folder.tar # extract files
