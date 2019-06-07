13cf8672ca21ec1d74d54adcdccb8905a488c128
Spencer White <swhite215@live.com>
2019-06-07 10:25:06 -0400

    example diff commands

---
+++ bash/comparing.sh
@@ -0,0 +1,11 @@
+
+# Example DIFF Commands, tools for comparing files
+
+diff # compare two files
+diff fileOne fileTwo
+
+sdiff # side by side comparison
+sdiff fileOne fileTwo
+
+vimdiff #highlight differences in vim
+vimdiff fileOne fileTwo
