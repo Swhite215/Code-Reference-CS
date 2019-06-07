af9ac658275ba65d2de54d230894ce39a35d06ea
Spencer White <swhite215@live.com>
2019-06-07 10:38:42 -0400

    added grep commands

---
+++ bash/searching.sh
@@ -0,0 +1,9 @@
+# Example GREP Commands, a tool for searching files
+
+grep pattern file
+
+grep -i pattern file # ignores case
+grep -c pattern file # count of lines where pattern occurs in file
+grep -v pattern file # shows lines where pattern does not exist
+grep -n pattern file # shows line numbers
+
