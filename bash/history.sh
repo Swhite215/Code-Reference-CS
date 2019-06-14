ac569142971891477338d71eeb3276bcd37e7449
Spencer White <swhite215@live.com>
2019-06-14 09:19:18 -0400

    added history command and re-execution

---
+++ bash/history.sh
@@ -0,0 +1,6 @@
+# Example HISTORY Commands, tools for seeing and re-executing command history
+
+history # lists up to HISTSIZE commands stored in memory
+!N # repeat command line number N
+!! # repeat previous command
+!string # repeat most recent command starting with "string"
