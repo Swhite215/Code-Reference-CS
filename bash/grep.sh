2038abc3f41b423694024eb1df95f65784687ca0
Spencer White <swhite215@live.com>
2019-08-22 11:08:19 -0400

    neat trick for searching dictionary

--- bash/grep.sh
+++ bash/grep.sh
@@ -7,3 +7,5 @@ grep -c pattern file # count of lines where pattern occurs in file
 grep -v pattern file # shows lines where pattern does not exist
 grep -n pattern file # shows line numbers
 
+grep -i '^....$' /usr/share/dict/words # neat trick to search dictionary for similar words
+
