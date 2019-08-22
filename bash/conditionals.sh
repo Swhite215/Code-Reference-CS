c79fb27cc7d6b500a96892d438f21a5c4e646b1e
Spencer White <swhite215@live.com>
2019-08-22 11:35:58 -0400

    file expressions for tests in conditionals

--- bash/conditionals.sh
+++ bash/conditionals.sh
@@ -1,1 +1,38 @@
-if [ condition ]; then # Execute code if true; else # Execute code if false; fi 
+# Example Conditional Flow with IF
+
+
+if [ condition ]; then 
+  # Execute code if true
+else 
+  # Execute code if false;
+fi 
+
+FILE=~/basic.sh
+
+if [ -e "$FILE" ]; then
+	
+	if [ -f "$FILE" ]; then
+		echo "$FILE is a regular file."
+	fi
+
+	if [ -d "$FILE" ]; then
+		echo "$FILE is a directory."
+	fi
+	
+	if [ -r "$FILE" ]; then
+		echo "$FILE is a readable."
+	fi
+
+	if [ -w "$FILE" ]; then
+		echo "$FILE is a writable."
+	fi
+
+	if [ -x "$FILE" ]; then
+		echo "$FILE is a executable."
+	fi	
+
+else
+	echo "$FILE does not exist!"
+	exit 1
+fi
+
