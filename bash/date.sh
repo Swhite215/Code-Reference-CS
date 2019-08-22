d503e7afecf56301726f45a1504e01b2eb1d5a0f
Spencer White <swhite215@live.com>
2019-08-22 10:12:55 -0400

    date examples

---
+++ bash/date.sh
@@ -0,0 +1,9 @@
+# Example DATE Commands, a tool for setting and displaying date
+
+
+date #displays the date - Thu Aug 22 10:07:32 EDT 2019
+date -R # - Thu Aug 22 10:07:32 EDT 2019
+date -r filename
+date -u # - Thu Aug 22 14:08:34 UTC 2019
+date "+DATE: %Y-%m-%d" # - DATE: 2019-08-22
+date "+TIME: %H:%M:%S" # - TIME: 10:10:45
