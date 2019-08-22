0aa6d0ac83b63434abccda88d216429d3bf6d0bd
Spencer White <swhite215@live.com>
2019-08-22 10:11:50 -0400

    uses of date

--- bash/basic.sh
+++ bash/basic.sh
@@ -16,6 +16,13 @@ cp -r source_directory destination
 cp source_file destination_file
 cp source_file1 source_filen dest_dir
 
+date #displays the date - Thu Aug 22 10:07:32 EDT 2019
+date -R # - Thu Aug 22 10:07:32 EDT 2019
+date -r filename
+date -u # - Thu Aug 22 14:08:34 UTC 2019
+date "+DATE: %Y-%m-%d" # - DATE: 2019-08-22
+date "+TIME: %H:%M:%S" # - TIME: 10:10:45
+
 du # disk usage of all files in current directory
 du file
 
