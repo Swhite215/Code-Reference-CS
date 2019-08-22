df1b6dad12a7e6a78ecddec364702b5eaed9225b
Spencer White <swhite215@live.com>
2019-08-22 10:19:57 -0400

    cal and ncal examples

---
+++ bash/cal.sh
@@ -0,0 +1,11 @@
+# Example CAL Commands, a tool for displaying a calendar
+
+cal
+cal -m 1 # Displays month (1 - 12)
+cal -y 2020 # Displays entire year calendar
+cal -3 # Displays previous, current, and next month
+
+ncal
+ncal -J # Julian Calendar
+ncal -e # Date of Easter
+ncal -w # Number of week below column
