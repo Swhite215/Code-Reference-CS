ae3fc8f9f547b88198f063fb2b2171273b55df5a
Spencer White <swhite215@live.com>
2019-08-22 11:43:57 -0400

    some string expressions for tests in conditionals

--- bash/conditionals.sh
+++ bash/conditionals.sh
@@ -36,3 +36,20 @@ else
 	exit 1
 fi
 
+ANSWER="maybe"
+
+if [ -z "$ANSWER" ]; then
+	echo "There is no answer." >&2
+	exit 1
+fi
+
+if [ "$ANSWER" = "yes" ]; then
+	echo "The answer is yes!"
+elif [ "$ANSWER" = "no" ]; then
+	echo "The answer is no!"
+elif [ "$ANSWER" = "maybe" ]; then
+	echo "The answer is maybe!"
+else
+	echo "The answer is UNKNOWN"
+fi
+
