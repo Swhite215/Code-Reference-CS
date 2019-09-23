18ee03e0ee8f2b2d4f9bb6420dcf6cd89e999083
Spencer White <swhite215@live.com>
2019-09-23 09:14:19 -0400

    added infinite loop for repeat

--- raspberry_pi/sense_hat_multi_color.py
+++ raspberry_pi/sense_hat_multi_color.py
@@ -5,4 +5,5 @@ sense = SenseHat()
 blue = (0, 0, 255)
 yellow = (255, 255, 0)
 
-sense.show_message("Raspberry Pi is awesome!", text_colour=yellow, back_colour=blue)
+while True:
+  sense.show_message("Raspberry Pi is awesome!", text_colour=yellow, back_colour=blue)
