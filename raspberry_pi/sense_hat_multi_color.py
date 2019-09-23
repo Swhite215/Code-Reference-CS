585e2542f004a930adb0f83847167f9cfa2b5de6
Spencer White <swhite215@live.com>
2019-09-23 09:13:19 -0400

    background and text color example

---
+++ raspberry_pi/sense_hat_multi_color.py
@@ -0,0 +1,8 @@
+from sense_hat import SenseHat
+
+sense = SenseHat()
+
+blue = (0, 0, 255)
+yellow = (255, 255, 0)
+
+sense.show_message("Raspberry Pi is awesome!", text_colour=yellow, back_colour=blue)
