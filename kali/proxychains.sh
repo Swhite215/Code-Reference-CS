516b6994c34a19cae0b97b94fe22b43533473575
Spencer White <swhit210@ford.com>
2019-11-19 14:15:47 -0500

    notes on proxychains and .conf file

---
+++ kali/proxychains.sh
@@ -0,0 +1,12 @@
+# Access Proxy File
+
+apt-get install proxychains
+
+nano /etc/proxychains.conf
+
+# Proxy Types - HTTP, SOCKS4, SOCKS5*
+
+# Proxy Options - dynamic_chain, strict_chain, random_chain
+# Proxy Options - uncomment to use chain
+
+proxychains browser server
