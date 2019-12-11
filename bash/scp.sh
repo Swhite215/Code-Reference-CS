9662ec73f0b4b064caf85ac6d65f9fa92a3cc0ca
Spencer White <swhit210@ford.com>
2019-12-11 15:52:12 -0500

    moved bash folder

--- bash/scp.sh
+++
@@ -1,5 +0,0 @@
-# Example SCP Commands, Secure File Copy, a program for copying files between computers
-
-scp file host:path # copy to
-scp host:path file #copy from
-scp -r directory host:path
--- bash/zip.sh
+++
@@ -1,6 +0,0 @@
-# Example GUNZIP and GZIP Commands, tools for compressing and decompressing files
-
-gzip # compressing files to save space
-gzip file
-
-gunzip file
