6f157471e45d650fddebcb8d9bb7dde63d167f6c
Spencer White <swhit210@ford.com>
2020-03-07 08:58:23 -0500

    azure cli commands for web apps

---
+++ cloud/code/webapp.sh
@@ -0,0 +1,5 @@
+# Start a Web Application - Azure CLI
+az webapp start --resource-group resourceGroup --name <name>
+
+# Stop a Web Application - Azure CLI
+az webapp stop --resource-group resourceGroup --name <name>
