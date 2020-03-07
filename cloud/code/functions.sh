42b4fcc49ee361be1636e942929740a12ef49286
Spencer White <swhit210@ford.com>
2020-03-07 13:37:47 -0500

    cli commands for azure functions

---
+++ cloud/code/functions.sh
@@ -0,0 +1,14 @@
+# Init a Azure Function App - CLI
+func init
+
+# Create a Azure Function in App - CLI 
+func new
+
+# Start Azure Functions - CLI
+func start
+
+# Publish Function App to Azure - CLI
+func azure functionapp publish <azure-function-app-name>
+
+# Create a Function App - Azure CLI
+az functionapp create --resource-group <resource-group) --name <function-app-name> --storage-account <storage-account> --runtime node --consumption-plan-location <location>
