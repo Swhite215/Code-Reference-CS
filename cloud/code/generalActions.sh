8fa3ccb51651adcef37f8c787e3ec1487c88231d
Spencer White <swhit210@ford.com>
2020-03-07 08:58:16 -0500

    general az cli commands

---
+++ cloud/code/generalActions.sh
@@ -0,0 +1,8 @@
+# List Azure Subscriptions - Azure CLI
+az account list --output table
+
+# List Azure Resource Groups - Azure CLI
+az group list --output table
+
+# List Azure Resources in Resource Group - Azure CLI
+az resource list --resource-group <resource-group>
