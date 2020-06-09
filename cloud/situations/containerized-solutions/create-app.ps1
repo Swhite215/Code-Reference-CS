cc803418f948d086c2342ab51c6c48a0e50a7355
Spencer White <swhit210@ford.com>
2020-06-09 11:59:05 -0400

    commands to build a simple dotnet web app

---
+++ cloud/situations/containerized-solutions/create-app.ps1
@@ -0,0 +1,14 @@
+# Makes the App Directory
+mkdir webapp
+
+# Enters App Directory
+cd webapp
+
+# Create a New Dotnet MVC WebApp Project
+dotnet new mvc
+
+# Build the Project
+dotnet build
+
+# Run the Project
+dotnet run
